517e212f16af36671620c56cd67c594b2d7eca0c
ccm <1085542424@qq.com>
2021-05-10 15:39:03 +0800

    格子初始化

--- assets/Script/Astart/AStartLayer.ts
+++ assets/Script/Astart/AStartLayer.ts
@@ -9,7 +9,7 @@ const {ccclass, property, menu} = cc._decorator;
 
 @ccclass
 @menu('Script/AStart')
-export default class NewClass extends cc.Component {
+export default class AStartLayer extends cc.Component {
 
     @property(cc.Label)
     label: cc.Label = null;
@@ -18,32 +18,43 @@ export default class NewClass extends cc.Component {
     text: string = 'hello';
 
     // LIFE-CYCLE CALLBACKS:
+    pen: cc.Graphics = null;
+    drawNode: cc.Node = null;
 
     onLoad () {
-
+        this.drawNode = this.node.getChildByName("drawNode");
     }
 
     start () {
-        this.drawRect();
+        this.drawRect(10, 10);
     }
 
-    getPen(width : number = 1) {
-        let drawNode = this.node.getChildByName("drawNode");
-        let pen = drawNode.getComponent(cc.Graphics);
-        pen.lineWidth = width;
-        return pen;
+    initPen(width : number = 1) {
+        if (!this.pen) {
+            this.pen = this.drawNode.getComponent(cc.Graphics);
+        }
+        this.pen.lineWidth = width;
     }
-    drawRect() {
 
-        let pen = this.getPen(1);
-        pen.rect(200, 0, 100, 100);
-        pen.stroke();
-        pen.fill();
-        let pen2 = this.getPen(10);
-        pen2.rect(0, 0, 100, 100);
-        pen2.stroke();
-        pen2.fill();
+    drawRect(row : number, col : number) {
+        let totalWidth = cc.winSize.width;
+        let totalHeight = cc.winSize.height;
+        this.initPen(5);
+        let rowWidth = totalWidth / row;
+        let colHeight = totalHeight / col;
+        for (let i = 0; i < 10; i++) {
+            this.pen.moveTo(-totalWidth / 2, -i*colHeight + totalHeight / 2);
+            this.pen.lineTo(totalWidth / 2,-i*colHeight + totalHeight / 2);
+        }
+        this.pen.stroke();
+        for (let j = 0; j < 10; j++) {
+            this.pen.moveTo(j*rowWidth - totalWidth / 2, totalHeight / 2);
+            this.pen.lineTo(j*rowWidth - totalWidth / 2, -totalHeight / 2);
+        }
+        this.pen.stroke();
+
     }
+
 
     // update (dt) {}
 }
